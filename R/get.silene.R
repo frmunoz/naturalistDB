e3eb4eba83a6336f8178c06497ceb8dc9304df9f
frmunoz <fmunoz@univ-grenoble-alpes.fr>
2022-08-11 10:39:37 +0200

    Stop message if Silene file unavailable

--- R/get.silene.R
+++ R/get.silene.R
@@ -1,7 +1,10 @@
 get.silene <- function(x, 
-                       path.silene = "C:/Users/munozfra/Documents/Botanique/Localisations/CSV"
+                       path.silene = "C:/Users/munozfra/Documents/Botanique/Localisations/CSV",
                        dates = NULL,
                        locations = NULL)
 {
+  if(!file.exists(path.silene))
+    stop("No file with Silene data")
+  
   f.silene <- list.files(path.silene, pattern = ".csv")
 }
