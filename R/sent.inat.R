232cb562a0e393fd9eea8b5fadf58e33d1fe5098
frmunoz <fmunoz@univ-grenoble-alpes.fr>
2022-08-12 16:57:31 +0200

    Send observation to iNaturalist

--- R/sent.inat.R
+++
@@ -1,8 +0,0 @@
-sent.inat <- function(data) {
-  # See POST  options in https://www.inaturalist.org/pages/api+reference
-  tab <- data
-  
-  write.table(tab, file="observations.json")
-  
-  shell("ruby inaturalist-oauth-pkce-flow.rb")
-}
